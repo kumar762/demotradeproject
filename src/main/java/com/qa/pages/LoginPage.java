32477880044c88ebcc441aedd428ece85950e062
kumar762 <kumarveda89@gmail.com>
2020-02-19 13:28:22 +0530

    Added search page and login methods

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,33 @@
 package com.qa.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.utilities.BaseClass;
+
+public class LoginPage extends BaseClass{
+	//elements
+	
+		@FindBy(name="txtUserName")WebElement Login_username;
+		@FindBy(name="txtPassword")WebElement Login_password;
+		@FindBy(name="Submit")WebElement Login_login;
+		@FindBy(name="clear")WebElement Login_clear;
+		
+	//constructor
+		
+		public LoginPage ()
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+		//login method
+		
+		public void login(String uname,String pwd)
+		{
+			Login_username.sendKeys(uname);
+			Login_password.sendKeys(pwd);
+			Login_login.click();
+		}
 
 }
